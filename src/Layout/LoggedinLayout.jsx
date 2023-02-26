6e6629010698d0d674de00ce67ad3a08e23ff501
Hamza <hamzaghnaimm99@gmail.com>
2023-02-27 00:46:20 +0300

    #2

--- src/Layout/LoggedinLayout.jsx
+++ src/Layout/LoggedinLayout.jsx
@@ -1,6 +1,24 @@
 import React, { useEffect, useContext } from "react";
 import { useNavigate, Routes, Route, Navigate } from "react-router-dom";
 
-// import { AuthenticationService } from "../services";
-// import { MainRouter } from "../routers";
+import { AuthenticationService } from "../services/AuthService";
+import { MainRouter } from "../routers";
 import { UserContext } from "../context";
+
+const Wrapper = ({ Component }) => <Component />;
+
+const LoggedinLayout = () => {
+  const navigate = useNavigate();
+  const { setCurrentUser } = useContext(UserContext);
+
+  useEffect(() => {
+    const isLoggedIn = AuthenticationService.isLoggedIn();
+    if (!isLoggedIn) {
+      navigate(
+        `/login?returnUrl=${encodeURIComponent(
+          window.location.href.replace(window.location.origin, "")
+        )}`
+      );
+    } else setCurrentUser(AuthenticationService.getCurrentUser());
+  }, [navigate, setCurrentUser]);
+};
